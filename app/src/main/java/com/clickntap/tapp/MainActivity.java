dfa213b90084d06e6cdfca1858fe0c363420100d
Tonino Mendicino <tonino@clickntap.com>
2018-01-29 14:30:03 +0100

    First commit

---
+++ app/src/main/java/com/clickntap/tapp/MainActivity.java
@@ -0,0 +1,12 @@
+package com.clickntap.tapp;
+import android.os.Bundle;
+
+import com.clickntap.tap.TapActivity;
+
+public class MainActivity extends TapActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
